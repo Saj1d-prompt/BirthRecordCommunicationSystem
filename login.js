ddf1be01a579cd2f307eec440ec4ba0fccc8a0d6
Saj1d-prompt <sajidulislam2225@gmail.com>
2025-08-02 14:14:29 +0600

    minor changes

--- login.js
+++ login.js
@@ -3,8 +3,8 @@ function login(event){
     const userID = document.getElementById("userID").value.trim();
     const password = document.getElementById("password").value;
 
-    if(password == "patient"){
-        window.location.href = "patient_dashboard.html";
+    if(password == "parent"){
+        window.location.href = "parent_dashboard.html";
     }
     else if(password == "admin"){
         window.location.href = "admin_dashboard.html";
